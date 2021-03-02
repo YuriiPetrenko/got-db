342e460730ca0c5200ca8e6af6c2281fffb96c8d
yuriipetrenko <obzora6@gmail.com>
2021-03-02 23:49:08 +0200

    Updates

---
+++ precache-manifest.661e79012dbc32b4d5de2376263fd6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a1214755292e2f4481",
+    "url": "/got-db/static/css/main.0354067c.chunk.css"
+  },
+  {
+    "revision": "42a1214755292e2f4481",
+    "url": "/got-db/static/js/main.42a12147.chunk.js"
+  },
+  {
+    "revision": "87de03394cbe3b20f0f5",
+    "url": "/got-db/static/css/1.1c4dbbea.chunk.css"
+  },
+  {
+    "revision": "87de03394cbe3b20f0f5",
+    "url": "/got-db/static/js/1.87de0339.chunk.js"
+  },
+  {
+    "revision": "09f59aaa92f6ec4dbe4c",
+    "url": "/got-db/static/js/runtime~main.09f59aaa.js"
+  },
+  {
+    "revision": "e94635a24e9dcebbbb9bf8da8f0dcbb7",
+    "url": "/got-db/index.html"
+  }
+];
