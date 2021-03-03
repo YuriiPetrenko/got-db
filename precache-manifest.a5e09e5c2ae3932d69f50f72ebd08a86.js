4333883e0f31512c75e57f57d8c02e45f060c901
yuriipetrenko <obzora6@gmail.com>
2021-03-03 11:08:40 +0200

    Updates

---
+++ precache-manifest.a5e09e5c2ae3932d69f50f72ebd08a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b176987e84063af4757",
+    "url": "/got-db/static/css/main.8e0ec29d.chunk.css"
+  },
+  {
+    "revision": "9b176987e84063af4757",
+    "url": "/got-db/static/js/main.9b176987.chunk.js"
+  },
+  {
+    "revision": "6a0655a87a41411cf17b",
+    "url": "/got-db/static/css/1.e43c8ff6.chunk.css"
+  },
+  {
+    "revision": "6a0655a87a41411cf17b",
+    "url": "/got-db/static/js/1.6a0655a8.chunk.js"
+  },
+  {
+    "revision": "09f59aaa92f6ec4dbe4c",
+    "url": "/got-db/static/js/runtime~main.09f59aaa.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/got-db/static/media/err.22389cf4.jpg"
+  },
+  {
+    "revision": "02c19caab7fc5541f7476299c6aa3a7f",
+    "url": "/got-db/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/got-db/precache-manifest.661e79012dbc32b4d5de2376263fd6ad.js"
+  "/got-db/precache-manifest.a5e09e5c2ae3932d69f50f72ebd08a86.js"
 );
 
 workbox.clientsClaim();
